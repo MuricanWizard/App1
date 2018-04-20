56625512cfa15a2de8dfc8b2699fba100aeb0260
Diego Machado <diegomac1798@hotmail.com>
2018-04-19 23:15:10 -0500

    cool

--- TutorXAndroid/Resources/Resource.Designer.cs
+++ TutorXAndroid/Resources/Resource.Designer.cs
@@ -66,20 +66,17 @@ namespace TutorXAndroid
 			// aapt resource value: 0x7f050000
 			public const int ChooseAppointment = 2131034112;
 			
+			// aapt resource value: 0x7f050003
+			public const int Student = 2131034115;
+			
 			// aapt resource value: 0x7f050004
-			public const int Student = 2131034116;
+			public const int Tutor = 2131034116;
 			
 			// aapt resource value: 0x7f050005
-			public const int Tutor = 2131034117;
-			
-			// aapt resource value: 0x7f050006
-			public const int imageView1 = 2131034118;
+			public const int imageView1 = 2131034117;
 			
 			// aapt resource value: 0x7f050002
-			public const int linearLayout1 = 2131034114;
-			
-			// aapt resource value: 0x7f050003
-			public const int textView1 = 2131034115;
+			public const int textView1 = 2131034114;
 			
 			static Id()
 			{
